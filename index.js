9daf377be1c07c43b7ac7315bb83f067072a6115
Owyn <Owyn@users.noreply.github.com>
2018-03-07 19:37:08 +0300

    test release

---
+++ index.js
@@ -0,0 +1,20 @@
+// 97000033	ASpd	DOT	True	perc	60000	0	1.1	Lakan's Might			Each stack increases Attack Speed by $H_W_GOOD10%$COLOR_END.	icon_status.plusciriticalresist_tex	icon_status.plusciriticalresist_tex	True
+
+// 46753	StackCounter	Buff	True	seta	10000	0	46751	Shriek			When stacked, produces Hardened Bark.	icon_status.angerwarrior_tex	icon_status.angerwarrior_tex	True
+
+
+module.exports = function Lachelith_stacks_type_fix(d) {
+	
+	function aHook(event)
+	{
+		if (event.id == 97000033) 
+		{
+			event.id = 46753;
+		}
+		return true;
+	}
+
+	dispatch.hook('S_ABNORMALITY_BEGIN', 1, aHook)
+	dispatch.hook('S_ABNORMALITY_END', 1, aHook)
+
+}
